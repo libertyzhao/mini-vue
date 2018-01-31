08ba8113afbf530b1e3e38628ebd0362d70f0933
liberty__ <376968795@qq.com>
2018-01-31 19:44:04 +0800

    处理完响应式代码

--- src/index.js
+++ src/index.js
@@ -1,13 +1,12 @@
-import { defineReactive } from './reactive';
+import { defineProxy, defineReactive, Watcher, Computed } from './reactive';
 
 function LV(options){
-	Object.keys(options).forEach(key=>{
-		this[key] = options[key];
-	})
-	this.init();
+	this.init(options);
 }
 
-LV.prototype.init = function(){
+LV.prototype.init = function(options){
+	mergeOptions(this,options)
+	this.initMethod(this);
 	this.initRender(this);
 	this.initState(this);
 }
@@ -20,12 +19,59 @@ LV.prototype.initState = function(lv){
 	if(lv.data){
 		initData(lv);
 	}
+	if(lv.watch){
+		initWatch(lv);
+	}
+	if(lv.computed){
+		initComputed(lv)
+	}
+}
+
+LV.prototype.initMethod = function(lv){
+	var methods = lv.methods;
+	var computed = lv.computed;
+	var watch = lv.watch;
+	bindThis(lv.methods,lv);
+	bindThis(lv.computed,lv);
+	bindThis(lv.watch,lv);
 }
 
 function initData(lv){
 	let data = lv.data;
 	Object.keys(data).forEach(key=>{
+		defineProxy(lv,key,data);
 		defineReactive(data,key);
+	})
+}
+
+function initWatch(lv){
+	let watch = lv.watch;
+	Object.keys(watch).forEach(key=>{
+		let watcher = new Watcher(lv,watch,key);
+		watch[key].watcher = watcher;
+	})
+}
+
+function initComputed(lv){
+	let computed = lv.computed;
+	Object.keys(computed).forEach(key=>{
+		let computer = new Computed(lv,computed,key);
+		computed[key].computer = computer;
+		defineProxy(lv,key,lv.data);
+	})
+}
+
+function bindThis(methods,lv){
+	if(methods){
+		Object.keys(methods).forEach(name => {
+			methods[name] = methods[name].bind(lv);
+		});
+	}
+}
+
+function mergeOptions(lv,options){
+	Object.keys(options).forEach(key=>{
+		lv[key] = options[key];
 	})
 }
 
--- src/reactive.js
+++ src/reactive.js
@@ -1,16 +1,95 @@
-export function defineReactive(target,key){
-	target[`__${key}`] = target[key];
-	Object.defineProperty(target,key,{
-		get(){
-			// console.log('执行了get');
-			return target[`__${key}`];
-		},
-		set(value){
-			// console.log('执行了set');
-			target[`__${key}`] = value;
-			return value;
-		}
-	})
+export function defineProxy(lv, key, proxy) {
+  Object.defineProperty(lv, key, {
+    get() {
+      return proxy[key];
+    },
+    set(value) {
+      return (proxy[key] = value);
+    }
+  });
 }
 
- 
+export function defineReactive(target, key) {
+  var ob = new Observable(target[key]);
+  recursiveObj(target[key], ob);
+  Object.defineProperty(target, key, {
+    get() {
+      return ob.get();
+    },
+    set(value) {
+      return ob.set(value);
+    }
+  });
+  return ob;
+}
+
+//递归对象的所有属性，把它们再转换成observable
+function recursiveObj(obj, ob) {
+  if (Array.isArray(obj)) {
+    arrReactive(obj, ob);
+  } else if (typeof obj === "object") {
+    Object.keys(obj).forEach(item => {
+      defineReactive(obj, item);
+    });
+  }
+}
+
+var DepTarget = null; //被收集的watcher
+
+function Observable(value) {
+  this.value = value;
+  this.dep = [];
+  this.get = function() {
+    if (DepTarget) {
+      this.dep.push(DepTarget);
+    }
+    return this.value;
+  };
+  this.set = function(value) {
+    this.value = value;
+    this.notify(value);
+  };
+  this.notify = function(value) {
+    var deps = this.dep.slice();
+    for (var i = 0, length = deps.length; i < length; i++) {
+      deps[i](value);
+    }
+  };
+}
+
+export function Watcher(lv, watch, key) {
+  DepTarget = watch[key];
+  lv.data[key];
+  DepTarget = null;
+}
+
+export function Computed(lv, computed, key) {
+  lv.data = lv.data || {};
+  DepTarget = () => {
+    lv.data[key] = computed[key]();
+  };
+  computed[key]();
+  DepTarget = null;
+}
+
+function arrReactive(arr, ob) {
+  let arrProto = Object.create(Array.prototype);
+  let arrMethods = [
+    "push",
+    "pop",
+    "shift",
+    "unshift",
+    "splice",
+    "sort",
+    "reverse"
+  ];
+  let middleware = {};
+  arrMethods.forEach(method => {
+    middleware[method] = (...args) => {
+      Array.prototype[method].apply(arr, args);
+      ob.notify(arr);
+    };
+  });
+  middleware.__proto__ = arrProto;
+  arr.__proto__ = middleware;
+}
